2d9419797b1fe4f12efcb30cacc665ce98498682
Ishraq-R-Rahman <67277219+Ishraq-R-Rahman@users.noreply.github.com>
2024-03-27 00:06:06 -0700

    :poop:Issue with port

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 3000, // or any specific port you want to use
+    },
     test: {
         globals: true,
         environment: "jsdom",
