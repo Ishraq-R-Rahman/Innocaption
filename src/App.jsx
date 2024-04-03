4ee9fef156c9a0ba39fea8e3576db77e94703942
Ishraq-R-Rahman <67277219+Ishraq-R-Rahman@users.noreply.github.com>
2024-04-02 23:06:39 -0700

    :art:Home Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 function App() {
-
     return (
         <QueryClientProvider client={queryClient}>
-            <div>
-              Hello World
-            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/men" element={<h1>Men</h1>} />
+                    <Route path="/women" element={<h1>Women</h1>} />
+                </Routes>
+            </BrowserRouter>
         </QueryClientProvider>
     );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,79 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { motion } from "framer-motion";
+
+import "./Home.css";
+import { useMediaQuery } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+    const isMobile = useMediaQuery("(max-width:768px)");
+
+    const manImageUrl = "/src/assets/Man.jpg";
+    const womanImageUrl = "/src/assets/Woman.jpg";
+
+    const navigate = useNavigate();
+
+    const goToMain = (gender) => () => {
+        navigate(`/${gender}`); // Navigate to /about page
+    };
+
+    return (
+        <Container
+            fluid
+            className="container"
+            style={{ padding: 0, margin: 0, maxWidth: "100%" }}
+        >
+            <div
+                style={{
+                    position: "absolute",
+                    top: isMobile ? "50%" : "10%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    zIndex: 10,
+                    textAlign: "center",
+                }}
+            >
+                <h1>Innocaption</h1>
+            </div>
+            <Row style={{ flex: 1 }}>
+                <Col
+                    xs={12}
+                    md={6}
+                    className="column-home"
+                    style={{
+                        height: isMobile ? "50vh" : "100vh",
+                        backgroundImage: `url(${manImageUrl})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center top",
+                        color: "white",
+                    }}
+                    onClick={goToMain("men")}
+                >
+                    <motion.h2 className="gender" whileHover={{ scale: 1.1 }}>
+                        Men
+                    </motion.h2>
+                </Col>
+                <Col
+                    xs={12}
+                    md={6}
+                    className="column-home"
+                    style={{
+                        height: isMobile ? "50vh" : "100vh",
+                        backgroundImage: `url(${womanImageUrl})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center top",
+                        color: "white",
+                    }}
+                    onClick={goToMain("women")}
+                >
+                    <motion.h2 className="gender" whileHover={{ scale: 1.1 }}>
+                        Women
+                    </motion.h2>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Home;
